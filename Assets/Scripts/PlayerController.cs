8a692c99a3e3e668dae1bc2a17c9ff791438bf95
Asher Harvey-Smith <asherharveysmith@gmail.com>
2022-07-15 20:39:21 +0100

    add ITool and sample implementation WateringCan

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
 	private float speed = 0.1f;
 
+	private ITool tool = null;
+
 	// Start is called before the first frame update
 	void Start() {}
 
---
+++ Assets/Scripts/WateringCan.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WateringCan : ITool
+{
+    public void DoWorkOn(DiceTree dt)
+    {
+        Debug.Log("foobar");
+    }
+}
