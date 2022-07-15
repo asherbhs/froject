6ddd747b7ec9e585107901f44d011989d560b591
Asher Harvey-Smith <asherharveysmith@gmail.com>
2022-07-15 19:43:40 +0100

    initial player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+	private float speed = 0.1f;
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.Translate(
+			Input.GetAxis("Horizontal") * speed,
+			Input.GetAxis("Vertical")   * speed,
+			0
+		);
+	}
+}
